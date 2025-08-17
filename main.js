be4a5c72e18c35aed7fbcc9d5837ee7a7af4a526
Tergel Tuvshinjargal <ttergel@protonmail.com>
2025-08-17 11:39:41 +0800

    feat: determine computer's choice

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hi");
+let cpuChoice = Math.random();
+
+if (cpuChoice <= 0.33) {
+    console.log("Rock");
+} else if (cpuChoice >= 0.33 && cpuChoice <= 0.66) {
+    console.log("Paper");
+} else {
+    console.log("Scissors");
+}
