cb15616fb1ff769a41488715ca27fa1568d3bf7f
Tergel Tuvshinjargal <ttergel@protonmail.com>
2025-08-17 13:16:40 +0800

    feat: add function to play 5 rounds i.e 1 game

--- main.js
+++ main.js
@@ -16,10 +16,9 @@ function getUserChoice() {
 let userScore = 0;
 let cpuScore = 0;
 
-const userSelection = getUserChoice();
-const cpuSelection = getComputerChoice();
-
-function playRound(userChoice, cpuChoice) {
+function playRound() {
+    let userChoice = getUserChoice();
+    let cpuChoice = getComputerChoice();
     let result = (userChoice === "rock" && cpuChoice === "paper") ? 0
     : (userChoice === "paper" && cpuChoice === "scissors") ? 0
     : (userChoice === "scissors" && cpuChoice === "rock") ? 0
@@ -30,7 +29,18 @@ function playRound(userChoice, cpuChoice) {
     userScore += (result === 1) ? 1
     : (result === 2 ) ? 0.5
     : 0
+    cpuScore += (result === 0 ) ? 1
+    : (result === 2) ? 0.5
+    : 0 
     console.log((result === 1) ? "You win" : (result === 2) ? "Draw" : "You lose")
 }
 
-playRound(userSelection, cpuSelection);
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    console.log(`Your score is ${userScore} and the CPU score is ${cpuScore}`)
+    console.log((userScore > cpuScore) ? "You are the winner" : (userScore < cpuScore) ? "You lost. Try again next time!" : "Draw!")
+}
+
+playGame();
