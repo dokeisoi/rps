65511673bca9962a075c82367e00b4d5a891649d
Tergel Tuvshinjargal <ttergel@protonmail.com>
2025-08-17 11:52:14 +0800

    refactor: move computer choice into reusable function

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let cpuChoice = Math.random();
-
-if (cpuChoice <= 0.33) {
-    console.log("Rock");
-} else if (cpuChoice >= 0.33 && cpuChoice <= 0.66) {
-    console.log("Paper");
-} else {
-    console.log("Scissors");
+function getComputerChoice () {
+    let choiceSelector = Math.random();
+    let cpuChoice = (choiceSelector <= 0.333)
+        ? "Rock"
+        : (choiceSelector >= 0.333 && choiceSelector <= 0.666)
+        ? "Paper"
+        : "Scissors"
+    return cpuChoice;
 }
