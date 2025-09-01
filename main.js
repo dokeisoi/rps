20d82e407f8f891ce313b2823e47479d4ae627bc
Tergel Tuvshinjargal <ttergel@protonmail.com>
2025-09-01 20:25:17 +0800

    feat: link playRound to the UI

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const r = "rock";
-const p = "paper";
-const s = "scissors";
+const r = "Rock";
+const p = "Paper";
+const s = "Scissors";
 
 let userScore = 0;
 let cpuScore = 0;
+let cpuChoice = '';
+
+const userScoreDisplay = document.querySelector('#user-score');
+const cpuScoreDisplay = document.querySelector('#cpu-score');
+const cpuSelectionDisplay = document.querySelector('#cpu-selection');
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', () => {
+    playRound('Rock');
+    userScoreDisplay.textContent = `Your score: ${userScore}`;
+    cpuScoreDisplay.textContent = `CPU score: ${cpuScore}`;
+    cpuSelectionDisplay.textContent = `CPU chose: ${cpuChoice}`;
+});
+paper.addEventListener('click', () => {
+    playRound('Paper');
+    userScoreDisplay.textContent = `Your score: ${userScore}`;
+    cpuScoreDisplay.textContent = `CPU score: ${cpuScore}`;
+    cpuSelectionDisplay.textContent = `CPU chose: ${cpuChoice}`;
+});
+scissors.addEventListener('click', () => {
+    playRound('Scissors');
+    userScoreDisplay.textContent = `Your score: ${userScore}`;
+    cpuScoreDisplay.textContent = `CPU score: ${cpuScore}`;
+    cpuSelectionDisplay.textContent = `CPU chose: ${cpuChoice}`;
+});
 
 function getComputerChoice() {
     let choiceSelector = Math.random();
@@ -15,15 +43,10 @@ function getComputerChoice() {
     return cpuChoice;
 }
 
-function getUserChoice() {
-    let userChoice = prompt("Choose: ").toLowerCase();
-    return userChoice;
-}
-
-function playRound() {
-    let userChoice = getUserChoice();
-    let cpuChoice = getComputerChoice();
-    let result = (userChoice === cpuChoice) ? 0.5
+function playRound(input) {
+    let userChoice = input;
+    cpuChoice = getComputerChoice();
+    let result = (userChoice === cpuChoice) ? 2
     : (userChoice === r && cpuChoice === s) ||
     (userChoice === p && cpuChoice === r) ||
     (userChoice === s && cpuChoice === p) ? 1
@@ -32,9 +55,12 @@ function playRound() {
     userScore += (result === 1) ? 1
     : (result === 2 ) ? 0.5
     : 0
-    
+
     cpuScore += (result === 0 ) ? 1
     : (result === 2) ? 0.5
     : 0 
-    console.log((result === 1) ? "You win" : (result === 2) ? "Draw" : "You lose")
+
+    console.log((result === 1) ? `You win. Computer chose ${cpuChoice}`
+    : (result === 2) ? `Draw. Computer chose ${cpuChoice}`
+    : `You lose. Computer chose ${cpuChoice}`)
 }
