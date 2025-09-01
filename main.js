95a586d324ccbd2d71e9db55009a0ae7c72b02c0
Tergel Tuvshinjargal <ttergel@protonmail.com>
2025-09-01 19:19:12 +0800

    refactor: clean core game logic

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+const r = "rock";
+const p = "paper";
+const s = "scissors";
+
+let userScore = 0;
+let cpuScore = 0;
+
 function getComputerChoice() {
     let choiceSelector = Math.random();
     let cpuChoice = (choiceSelector <= 0.333)
-        ? "rock"
+        ? r
         : (choiceSelector >= 0.333 && choiceSelector <= 0.666)
-        ? "paper"
-        : "scissors"
+        ? p
+        : s
     return cpuChoice;
 }
 
@@ -13,34 +20,21 @@ function getUserChoice() {
     return userChoice;
 }
 
-let userScore = 0;
-let cpuScore = 0;
-
 function playRound() {
     let userChoice = getUserChoice();
     let cpuChoice = getComputerChoice();
-    let result = (userChoice === "rock" && cpuChoice === "paper") ? 0
-    : (userChoice === "paper" && cpuChoice === "scissors") ? 0
-    : (userChoice === "scissors" && cpuChoice === "rock") ? 0
-    : (userChoice === "rock" && cpuChoice === "scissors") ? 1
-    : (userChoice === "paper" && cpuChoice === "rock") ? 1
-    : (userChoice === "scissors" && cpuChoice === "paper") ? 1
-    : 2
+    let result = (userChoice === cpuChoice) ? 0.5
+    : (userChoice === r && cpuChoice === s) ||
+    (userChoice === p && cpuChoice === r) ||
+    (userChoice === s && cpuChoice === p) ? 1
+    : 0
+    
     userScore += (result === 1) ? 1
     : (result === 2 ) ? 0.5
     : 0
+    
     cpuScore += (result === 0 ) ? 1
     : (result === 2) ? 0.5
     : 0 
     console.log((result === 1) ? "You win" : (result === 2) ? "Draw" : "You lose")
 }
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    console.log(`Your score is ${userScore} and the CPU score is ${cpuScore}`)
-    console.log((userScore > cpuScore) ? "You are the winner" : (userScore < cpuScore) ? "You lost. Try again next time!" : "Draw!")
-}
-
-playGame();
